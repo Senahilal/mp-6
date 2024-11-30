e4ea3ad9991d6454fec15c5e2f1b1df1ff7020a4
Senahilal <senab@bu.edu>
2024-11-29 20:41:06 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// "useClient"
+
+import styled from "styled-components";
+import { useState } from "react";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
