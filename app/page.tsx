7b333ee3b83d67649a8ac5d5c317640851700f70
Senahilal <senab@bu.edu>
2024-11-29 20:44:31 -0500

    commented out unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 // "useClient"
 
-import styled from "styled-components";
-import { useState } from "react";
-import Link from "next/link";
+// import styled from "styled-components";
+// import { useState } from "react";
+// import Link from "next/link";
 
 export default function Home() {
   return (
